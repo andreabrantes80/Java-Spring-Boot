b83322b71f07bdde90db3afe1a900ee9c4ee727b
andreabrantes80 <meloabrantes@gmail.com>
2026-07-28 21:29:26 -0300

    construção e ajustes da feature consultas

--- api/src/main/java/med/voll/api/controller/ConsultaController.java
+++ api/src/main/java/med/voll/api/controller/ConsultaController.java
@@ -2,11 +2,11 @@ package med.voll.api.controller;
 
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
-import med.voll.api.domain.consulta.AgendaDeConsultas;
-import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
-import med.voll.api.domain.consulta.DadosCancelamentoConsulta;
-import med.voll.api.domain.consulta.DadosDetalhamentoConsulta;
+import med.voll.api.domain.consulta.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +17,9 @@ import org.springframework.web.bind.annotation.*;
 public class ConsultaController {
     @Autowired
     private AgendaDeConsultas agenda;
+
+    @Autowired
+    private ConsultaRepository consultaRepository;
     @PostMapping
     @Transactional
     public ResponseEntity agendar(@RequestBody @Valid DadosAgendamentoConsulta dados){
@@ -29,5 +32,25 @@ public class ConsultaController {
     public ResponseEntity cancelar(@RequestBody @Valid DadosCancelamentoConsulta dados) {
         agenda.cancelar(dados);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<DadosDetalhamentoConsulta>> listar(
+            @PageableDefault(size = 10, sort = "data") Pageable pageable) {
+
+        var page = consultaRepository
+                .findAll(pageable)
+                .map(DadosDetalhamentoConsulta::new);
+
+        return ResponseEntity.ok(page);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DadosDetalhamentoConsulta> detalhar(
+            @PathVariable Long id) {
+
+        var consulta = consultaRepository.getReferenceById(id);
+
+        return ResponseEntity.ok(new DadosDetalhamentoConsulta(consulta));
     }
 }
