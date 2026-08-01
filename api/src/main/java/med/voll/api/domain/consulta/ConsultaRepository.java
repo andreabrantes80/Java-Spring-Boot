55e0a68142499dd6e07b9d415328f978b75fd576
andreabrantes80 <meloabrantes@gmail.com>
2026-07-31 22:31:31 -0300

    Ajustes nas classes do spring

--- api/src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
+++ api/src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
@@ -1,5 +1,7 @@
 package med.voll.api.domain.consulta;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
@@ -8,4 +10,6 @@ public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
 
     boolean existsByPacienteIdAndDataBetween(Long idPaciente, LocalDateTime primeiroHorario, LocalDateTime ultimoHorario);
     boolean existsByMedicoIdAndData(Long idMedico, LocalDateTime data);
+
+    Page<Consulta> findByCanceladaFalse(Pageable pageable);
 }
