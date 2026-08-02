4580e23084d73a66761f9a3a646b45e69897b114
andreabrantes80 <meloabrantes@gmail.com>
2026-08-02 20:44:21 -0300

    Ajustes na classe service o zeca esqueceu da anotação

--- api/src/main/java/med/voll/api/infra/email/EmailService.java
+++ api/src/main/java/med/voll/api/infra/email/EmailService.java
@@ -2,11 +2,12 @@ package med.voll.api.infra.email;
 
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
+@Service
 public class EmailService {
 
     private final JavaMailSender mailSender;
-
     public EmailService(JavaMailSender mailSender) {
         this.mailSender = mailSender;
     }
