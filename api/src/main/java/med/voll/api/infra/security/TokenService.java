c2633ee0ab7d61fa46b4e74ceb59ee9473ff36e2
andreabrantes80 <meloabrantes@gmail.com>
2026-08-20 23:08:16 -0300

    Ajustes no token

--- api/src/main/java/med/voll/api/infra/security/TokenService.java
+++ api/src/main/java/med/voll/api/infra/security/TokenService.java
@@ -26,6 +26,7 @@ public class TokenService {
             return JWT.create()
                     .withIssuer("API Voll.med")
                     .withSubject(usuario.getLogin())
+                    .withClaim("id", usuario.getId())
                     .withClaim("role", usuario.getRole().name())
                     .withExpiresAt(dataExpiracao()) //seta um tempo de expiração do token
                     .sign(algoritmo);
