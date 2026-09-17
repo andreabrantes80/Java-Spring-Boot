fc7bedd92e9e157663e9d40280dd2a7297a97a30
andreabrantes80 <meloabrantes@gmail.com>
2026-09-16 23:43:48 -0300

    Ajustes no websocket

--- api/src/main/java/med/voll/api/infra/websocket/WebSocketConfig.java
+++ api/src/main/java/med/voll/api/infra/websocket/WebSocketConfig.java
@@ -18,6 +18,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/senhas-websocket").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/senhas-websocket").setAllowedOriginPatterns("*").withSockJS();
     }
 }
